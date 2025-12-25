9a43ad6835a0b69735390cea8554df94b217dd4a
Marek Ledworowski <fotelpl@gmail.com>
2025-12-25 20:29:41 +0100

    updated tests and logs

--- tests/filesystem_dirents/code/fs_test.cpp
+++ tests/filesystem_dirents/code/fs_test.cpp
@@ -53,47 +53,47 @@ void DumpDirents(int fd, int buffer_size, s64 offset) {
 }
 
 void RunTests() {
-  RegenerateDir("/data/therapist");
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname01", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname02", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname03", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname04", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname05", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname06", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname07", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname08", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname09", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname10", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname11", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname12", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname13", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname14", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname15", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname16", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname17", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname18", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname19", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname10", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname21", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname22", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname23", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname24", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname25", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname26", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname27", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname28", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname29", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname30", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname31", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname32", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname33", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname34", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname35", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname36", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname37", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname38", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname39", O_CREAT | O_WRONLY | O_TRUNC, 0777));
-  sceKernelClose(sceKernelOpen("/data/therapist/filewithaverylongname40", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  RegenerateDir("/data/enderman");
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname01", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname02", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname03", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname04", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname05", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname06", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname07", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname08", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname09", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname10", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname11", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname12", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname13", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname14", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname15", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname16", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname17", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname18", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname19", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname10", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname21", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname22", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname23", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname24", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname25", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname26", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname27", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname28", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname29", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname30", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname31", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname32", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname33", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname34", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname35", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname36", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname37", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname38", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname39", O_CREAT | O_WRONLY | O_TRUNC, 0777));
+  sceKernelClose(sceKernelOpen("/data/enderman/filewithaverylongname40", O_CREAT | O_WRONLY | O_TRUNC, 0777));
 
 #define FUCKING_LSEEK_OFFSET 1024
 
@@ -101,7 +101,7 @@ void RunTests() {
   Log("Dump normal directory");
   Log("---------------------");
 
-  int fd = sceKernelOpen("/data/therapist", O_DIRECTORY | O_RDONLY, 0777);
+  int fd = sceKernelOpen("/data/enderman", O_DIRECTORY | O_RDONLY, 0777);
   Log("Directory opened with fd=", fd);
 
   Log("LSeek END+0=", sceKernelLseek(fd, 0, 2));
@@ -109,22 +109,24 @@ void RunTests() {
   Log("LSeek END+100=", sceKernelLseek(fd, 100, 2));
   Log("LSeek END-100000=", sceKernelLseek(fd, -100000, 2));
 
-  Log("TEST: /data/therapist, 512 buffer, 0 offset");
+  Log("TEST: /data/enderman, 512 buffer, 0 offset");
   DumpDirents(fd, 512, 0);
-  Log("TEST: /data/therapist, 512 buffer, 5 offset");
+  Log("TEST: /data/enderman, 512 buffer, 5 offset");
   DumpDirents(fd, 512, 5);
-  //   Log("TEST: /data/therapist, 550 buffer, 0 offset");
-  //   DumpDirents(fd, 550, 0);
-  //   Log("TEST: /data/therapist, 550 buffer, 5 offset");
-  //   DumpDirents(fd, 550, 5);
-  Log("TEST: /data/therapist, 768 buffer, 0 offset");
+  Log("TEST: /data/enderman, 512 buffer, 30 offset");
+  DumpDirents(fd, 512, 30);
+  Log("TEST: /data/enderman, 768 buffer, 0 offset");
   DumpDirents(fd, 768, 0);
-  Log("TEST: /data/therapist, 768 buffer, 5 offset");
+  Log("TEST: /data/enderman, 768 buffer, 5 offset");
   DumpDirents(fd, 768, 5);
-  //   Log("TEST: /data/therapist, 1024 buffer, 0 offset");
-  //   DumpDirents(fd, 1024, 0);
-  //   Log("TEST: /data/therapist, 1024 buffer, 5 offset");
-  //   DumpDirents(fd, 1024, 5);
+  Log("TEST: /data/enderman, 768 buffer, 30 offset");
+  DumpDirents(fd, 768, 5);
+  Log("TEST: /data/enderman, 1024 buffer, 0 offset");
+  DumpDirents(fd, 1024, 0);
+  Log("TEST: /data/enderman, 1024 buffer, 5 offset");
+  DumpDirents(fd, 1024, 5);
+  Log("TEST: /data/enderman, 1024 buffer, 30 offset");
+  DumpDirents(fd, 1024, 30);
 
   sceKernelClose(fd);
 
@@ -140,6 +142,7 @@ void RunTests() {
   Log("LSeek END+100=", sceKernelLseek(fd, 100, 2));
   Log("LSeek END-100000=", sceKernelLseek(fd, -100000, 2));
 
+  // These aren't really needed, it's assumed PFS is only read in 65536 chunks
   //   Log("TEST: /app0/assets/misc, 512 buffer, 0 offset");
   //   DumpDirents(fd, 512, 0);
   //   Log("TEST: /app0/assets/misc, 512 buffer, 5 offset");
@@ -152,10 +155,13 @@ void RunTests() {
   //   DumpDirents(fd, 768, 0);
   //   Log("TEST: /app0/assets/misc, 768 buffer, 5 offset");
   //   DumpDirents(fd, 768, 5);
+
   Log("TEST: /app0/assets/misc, 65536 buffer, 0 offset");
   DumpDirents(fd, 65536, 0);
   Log("TEST: /app0/assets/misc, 65536 buffer, 5 offset");
   DumpDirents(fd, 65536, 5);
+  Log("TEST: /app0/assets/misc, 65536 buffer, 30 offset");
+  DumpDirents(fd, 65536, 30);
 
   sceKernelClose(fd);
 }
